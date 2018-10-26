f2f30f67ceac6911941fec7bbe4b9623ba2356ad
105058031 <gabor.dubniczki@ge.com>
2018-10-26 15:16:17 +0100

    Event handler on highcharts points

--- dist/src/px-sample-dashboard.js
+++ dist/src/px-sample-dashboard.js
@@ -1,8 +1,10 @@
-"use strict";/**
+"use strict";
+/**
  * @license
  * Copyright (c) 2018, General Electric
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
+
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -13,10 +15,61 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */(function(){Polymer({is:"px-sample-dashboard",properties:{title:{type:String,value:""},
+ */(function(){Polymer({
+	 is:"px-sample-dashboard",
+properties:{
+	title:{
+			type:String,
+			value:""},
+	dropItems:{
+		 type:Array,
+		 value: function value(){
+			let now = new Date();
 
- chartData:{type:Array,value:function value(){return[
-{"timeStamp":1227232494001,"y0":"6.44","y1":"-3.96","y2":"-7.89","y3":"4.26"},
+			let onejan = new Date(now.getFullYear(), 0, 1);
+
+			var week = 0;
+
+			week = Math.ceil( (((now - onejan) / 86400000) + onejan.getDay() + 1) / 7 );
+
+			var stringent = "FW" + week;
+
+			var damphair = []			
+			damphair[0] = { "key" : String(week), "val" : stringent, "selected": "true" }
+			for (var i = 1;
+ i < 6;
+ i++) {
+			stringent = "FW" + String(week-i);
+
+			damphair[i] = { "key" : String(week-i), "val" : stringent }
+			}
+			console.log(damphair);
+
+			return damphair;
+
+								}
+	 },
+	 oneURL:{
+		type: String,
+		value: function value() {
+		let now = new Date();
+
+		let onejan = new Date(now.getFullYear(), 0, 1);
+
+		var week = 0;
+
+		week = Math.ceil( (((now - onejan) / 86400000) + onejan.getDay() + 1) / 7 );
+
+		return "https://fbororestapi.run.aws-usw02-pr.ice.predix.io/VTStock/"+String(week)+"/";
+
+		}
+	 },
+ chartData:{
+	 type:Array,
+	 value:function value()
+	 {
+		 return[
+{"timeStamp":1227232494001,"y0":"6.44","y1":"-3.96","y2":"-7.89","y3":"4.26"},	
 {"timeStamp":1228472111570,"y0":"6.31","y1":"-4.38","y2":"-7.60","y3":"3.65"},
 {"timeStamp":1229711729139,"y0":"5.75","y1":"-4.17","y2":"-7.47","y3":"3.90"},
 {"timeStamp":1230951346708,"y0":"5.96","y1":"-4.24","y2":"-6.78","y3":"4.15"},
@@ -240,10 +293,95 @@
 handleResponse: function(e)
 { 	if (e.detail.response!=null)	
 		{
-	this.SV = e.detail.response["0"].To_Stock;
+	this.AC = String(Math.round(Number(e.detail.response["0"].ATo_Stock)/1000));
+	this.SC = String(Math.round(Number(e.detail.response["0"].PTo_Stock)/1000));
+	this.P = String(Math.round(Number(e.detail.response["0"].varPercent)*1000))/10;
+	this.AV = String(Math.round(Number(e.detail.response["0"].absVar)/1000));
+	this.NV = String(Math.round(Number(e.detail.response["0"].netVar)/1000));
+	this.AP = String(Math.round(Number(e.detail.response["0"].absVar)/Number(e.detail.response["0"].PTo_Stock)*1000))/10;
 		}
 },
+handleChart: function(e)
+{ 	if (e.detail.response!=null)	
+		{
+			this.categoryList = []
+			this.ajaxResponse = []
+		for (var row in e.detail.response)
+			{
+	//#this.ajaxResponse.push(String(Math.round(Number(e.detail.response["0"].ATo_Stock)/1000));
+	this.categoryList.push(row.FW);
+			}
+		this.ajaxResponse=[{
+"type":"column",
+"name":"Neg",
+"color":"#FFA500",
+"data":
+[[e.detail.response[0].FW,-1239.83],
+[e.detail.response[1].FW,-98177.21],
+[e.detail.response[2].FW,-34093.73],
+[e.detail.response[3].FW,-26576.36],
+[e.detail.response[4].FW,-19012.86],
+[e.detail.response[5].FW,-40161.84],
+[e.detail.response[6].FW,-38917.31],
+[e.detail.response[7].FW,-99729.31]
+] }, 
+{
+"type":"column",
+"name":"Pos",
+"color":"#66CCCC",
+"data":
+[["FW37",0],
+["FW38",47573.82],
+["FW39",20532.56],
+["FW40",21828.94],
+["FW41",11884.34],
+["FW42",7025.33],
+["FW43",8366.91],
+["FW44",27196.02]
+] }, 
+{
+"type":"spline",
+"color":"#FF0000",
+"yAxis":1,
+"name":"Variance",
+"data":
+[["FW37",-3.3],
+["FW38",-9.8],
+["FW39",-3.8],
+["FW40",-1.8],
+["FW41",-3.7],
+["FW42",-6.9],
+["FW43",-8.3],
+["FW44",-12.9]
+] } ]
+		}
+},
+dropdownChanged: function(e)
+{
+	console.log(this.$.zeDrop.selected);
+},
+selectChanged: function(e)
+{
+	console.log("FW" + this.$.zeDrop.selected + " has been selected");
+	this.$.VAR.url = "https://fbororestapi.run.aws-usw02-pr.ice.predix.io/VarStats/"+String(this.$.zeDrop.selected)+"/";
+	this.$.VAR.generateRequest();
+},
+
 attached:function attached(){this.listen(document.querySelector("#cb"),"selected-changed","updateValues")},
 detached:function detached(){this.unlisten(document.querySelector("#cb"),"selected-changed")},
-updateValues:function updateValues(){var gauges=document.querySelectorAll("px-gauge");gauges.forEach(function(gauge){gauge.set("value",Math.floor(Math.random()*100))});var keyvalues=document.querySelectorAll("px-key-value-pair");keyvalues.forEach(function(keyvalue){keyvalue.set("value",Math.floor(Math.random()*100))})}})})();
+updateValues:function updateValues(){
+	var gauges=document.querySelectorAll("px-gauge");
+
+	gauges.forEach(function(gauge){gauge.set("value",Math.floor(Math.random()*100))});
+
+	var keyvalues=document.querySelectorAll("px-key-value-pair");
+keyvalues.forEach(function(keyvalue){keyvalue.set("value",Math.floor(Math.random()*100))})},
+ready: function() {
+        this.$.VAR.generateRequest();
+		this.$.sec.generateRequest();
+		
+
+    }
+	})})();
+
 //# sourceMappingURL=px-sample-dashboard.js.map
